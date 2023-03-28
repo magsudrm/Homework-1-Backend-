a27c249330be3126aaab543151b8581f5a4b3d2b
unknown <magsudrm@code.edu.az>
2023-03-29 01:38:56 +0400

    Project ended

---
+++ Homework 1/Homework 1/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_1.Controllers
+{
+	public class ProductController : Controller
+	{
+        public ActionResult Detail(int id, string brand, string category, decimal price)
+        {
+            ViewBag.Id = id;
+            ViewBag.Brand = brand;
+            ViewBag.Category = category;
+            ViewBag.Price = price;
+            return View();
+        }
+
+    }
+}
